29510588659b3de31079e938b0182e24834cb380
dani36i9@stud.kea.dk <dani36i9@stud.kea.dk>
2022-05-22 13:50:39 +0200

    Changes by veronica

--- src/record/RecordTraining.java
+++ src/record/RecordTraining.java
@@ -4,66 +4,27 @@ import enums.AgeGroup;
 
 import java.time.LocalDate;
 
-public class RecordTraining implements Comparable<RecordTraining> {
-  private String name;
-  private AgeGroup ageGroup;
-  private int timeInSeconds;
-  private LocalDate date;
+public class RecordTraining extends Record implements Comparable<RecordTraining> {
   
-  // String name, int timeInSeconds, LocalDate date, int placement, String convention
+  
   public RecordTraining(String name, AgeGroup ageGroup, int timeInSeconds, LocalDate date) {
-    setName(name);
-    setAgeGroup(ageGroup);
-    setTimeInSeconds(timeInSeconds);
-    setDate(date);
+    super(name, ageGroup, timeInSeconds, date);
   }
-  
-  public void setAgeGroup(AgeGroup ageGroup) {
-    this.ageGroup = ageGroup;
+
+
+  @Override
+  public int compareTo(RecordTraining other) {
+    return this.getTimeInSeconds() - other.getTimeInSeconds();
   }
-  
-  public AgeGroup getAgeGroup() {
-    return ageGroup;
-  }
-  
-  public void setDate(LocalDate date) {
-    this.date = date;
-  }
-  
-  public LocalDate getDate() {
-    return date;
-  }
-  
-  
-  public void setName(String name) {
-    this.name = name;
-  }
-  
-  public String getName() {
-    return name;
-  }
-  
-  public void setTimeInSeconds(int timeInSeconds) {
-    this.timeInSeconds = timeInSeconds;
-  }
-  
-  public int getTimeInSeconds() {
-    return timeInSeconds;
-  }
-  
+
   @Override
   public boolean equals(Object obj) {
     RecordTraining other = (RecordTraining) obj;
-    
-    return this.name.equals(other.name) &&
-           this.ageGroup == other.ageGroup &&
-           this.timeInSeconds == other.timeInSeconds &&
-           this.date.equals(other.date);
-  }
-  
-  @Override
-  public int compareTo(RecordTraining other) {
-    return this.timeInSeconds - other.timeInSeconds;
+
+    return this.getName().equals(other.getName()) &&
+           this.getAgeGroup() == other.getAgeGroup() &&
+           this.getTimeInSeconds() == other.getTimeInSeconds() &&
+           this.getDate().equals(other.getDate());
   }
   
 }
